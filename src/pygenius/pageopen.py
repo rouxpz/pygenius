c1e1b3c88e8fb07d300136e669ebb783e3c64de2
Roopa Vasudevan <roopa.vasudevan@gmail.com>
2014-07-15 17:37:29 -0400

    update urllib2 settings

--- src/pygenius/pageopen.py
+++ src/pygenius/pageopen.py
@@ -4,6 +4,7 @@ from bs4 import BeautifulSoup
 def openPage(url):
 
 	opener = urllib2.build_opener()
+	opener.addheaders = [('User-Agent', 'Mozilla/5.0')]
 	page = opener.open(url)
 	soup = BeautifulSoup(page, from_encoding="utf-8")
 
